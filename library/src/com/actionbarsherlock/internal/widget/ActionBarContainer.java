ecc35989e540439162275c9377f52c7ab55d6765
Jake Wharton <jakewharton@gmail.com>
2011-06-21 20:25:48 -0400

    Finish ActionBarContainer.

--- library/src/com/actionbarsherlock/internal/widget/ActionBarContainer.java
+++ library/src/com/actionbarsherlock/internal/widget/ActionBarContainer.java
@@ -4,6 +4,8 @@ import com.actionbarsherlock.R;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 public class ActionBarContainer extends FrameLayout {
@@ -20,4 +22,19 @@ public class ActionBarContainer extends FrameLayout {
 		a.recycle();
 	}
 	
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		return mIsTransitioning || super.onInterceptTouchEvent(ev);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		super.onTouchEvent(event);
+		return true;
+	}
+	
+	public void setTransitioning(boolean transitioning) {
+		mIsTransitioning = transitioning;
+		setDescendantFocusability(transitioning ? ViewGroup.FOCUS_BLOCK_DESCENDANTS : ViewGroup.FOCUS_AFTER_DESCENDANTS);
+	}
 }
